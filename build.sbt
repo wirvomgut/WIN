4bef624622eb59ccf2c7169114f78adb40d7cd0e
razem-io <mail@j-liebl.de>
2016-04-20 16:30:28 +0200

    implemented jquery

--- build.sbt
+++ build.sbt
@@ -8,4 +8,12 @@ scalaVersion := "2.11.8"
 
 scalaJSUseRhino in Global := false
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
+skip in packageJSDependencies := false
+
+persistLauncher in Compile := true
+
+persistLauncher in Test := false
+
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
+
+jsDependencies += "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
--- src/main/scala/wirvomgut/TutorialApp.scala
+++ src/main/scala/wirvomgut/TutorialApp.scala
@@ -1,25 +1,27 @@
 package wirvomgut
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
+import org.scalajs.jquery.jQuery
 
 import scala.scalajs.js.annotation.JSExport
 
 object TutorialApp extends JSApp {
   @JSExport
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    appendPar("You clicked the button!")
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def appendPar(text: String): Unit = {
+    jQuery("body").append("<p>" + text + "</p>")
+  }
+
+  def setupUI(): Unit = {
+    jQuery("body").append("<p>Hello World</p>")
+    jQuery("body").append("""<button id="click-me-button" type="button"">Click me!</button>""")
+    jQuery("#click-me-button").click(addClickedMessage _)
   }
 
   def main(): Unit = {
-    appendPar(document.body, "Hello World")
+    jQuery(setupUI _)
   }
 }
